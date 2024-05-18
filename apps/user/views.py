527c5039b4b3cd2e9c6f570adb0c0ded8a8adb02
liousa <m15827422856@163.com>
2024-05-18 15:05:24 +0800

    增加自动化测试平台home页面，增加form机制过滤减少数据库压力

--- apps/user/views.py
+++ apps/user/views.py
@@ -9,6 +9,8 @@ from django.contrib.auth.decorators import login_required
 from .models import UserProfile
 from django.views.generic.base import View
 
+from .forms import LoginForm
+
 # Create your views here.
 class CustomBackend(ModelBackend):
     def authenticate(self, request, username=None, password=None, **kwargs):
@@ -23,17 +25,22 @@ class LoginView(View):
     def get(self,request):
         return render(request, "login.html", {})
     def post(self,request):
-        user_name = request.POST.get("username", "")
-        pass_word = request.POST.get("password", "")
-        user = authenticate(request, username=user_name, password=pass_word)
-        if user is not None:
-            login(request, user)
-            return render(request, 'index.html')
+        login_form=LoginForm(request.POST)
+        if login_form.is_valid():
+
+            user_name = request.POST.get("username", "")
+            pass_word = request.POST.get("password", "")
+            user = authenticate(request, username=user_name, password=pass_word)
+            if user is not None:
+                login(request, user)
+                return render(request, 'index.html')
 
         else:
             return render(request, 'login.html', {'error': '非法登录'})
 
 
+
+##该方法可删除
 def user_login(request):
     if request.method == "POST":
         user_name = request.POST.get("username", "")
